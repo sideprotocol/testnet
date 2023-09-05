45e9a9d3d305baebb38eaa61c3e1b5f15b6f0eaa
Amit Yadav <amy29981@gmail.com>
2023-09-05 11:39:26 +0530

    testnet config ics101

--- shambhala/ics100/config.ts
+++ shambhala/ics100/config.ts
@@ -15,55 +15,95 @@
  */
 
 const config = {
-    sideChain: {
-      dev: {
-        contract:
-          'side1gg6f95cymcfrfzhpek7cf5wl53t5kng52cd2m0krgdlu8k58vd8qzv89wl',
-        counterparties: [
-          {
-            chainID: 'osmo-test-5',
-            sourceChannel: 'channel-21',
-          },
-          {
-            chainID: 'injective-888',
-            sourceChannel: 'channel-18',
-          },
-        ],
-      },
-      testnet: {},
-      main: {},
+  sideChain: {
+    dev: {
+      contract:
+        'side1gg6f95cymcfrfzhpek7cf5wl53t5kng52cd2m0krgdlu8k58vd8qzv89wl',
+      counterparties: [
+        {
+          chainID: 'osmo-test-5',
+          sourceChannel: 'channel-21',
+        },
+        {
+          chainID: 'injective-888',
+          sourceChannel: 'channel-18',
+        },
+      ],
     },
-    osmoChain: {
-      dev: {
-        contract:
-          'osmo1ahyxkyz6m7nk774g3x5amfrcznn06fw5zywedxtqg2hw8cgf9gks2rhgnl',
-        counterparties: [
-          {
-            chainID: 'injective-888',
-            sourceChannel: 'channel-1524',
-          },
-          {
-            chainID: 'side-devnet-1',
-            sourceChannel: 'channel-1523',
-          },
-        ],
-      },
-      main: {},
+    testnet: {
+      contract:
+        'side1eyfccmjm6732k7wp4p6gdjwhxjwsvje44j0hfx8nkgrm8fs7vqfstzz0ej',
+      counterparties: [
+        {
+          chainID: 'osmo-test-5',
+          sourceChannel: 'channel-?',
+        },
+        {
+          chainID: 'injective-888',
+          sourceChannel: 'channel-5',
+        },
+      ],
     },
-    injective: {
-      dev: {
-        contract: 'inj13gttmee75m22058kcnsua3yq8uhk9lwkmyurer',
-        counterparties: [
-          {
-            chainID: 'osmo-test-5',
-            sourceChannel: 'channel-100',
-          },
-          {
-            chainID: 'side-devnet-1',
-            sourceChannel: 'channel-99',
-          },
-        ],
-      },
-      main: {},
+    main: {},
+  },
+  osmoChain: {
+    dev: {
+      contract:
+        'osmo1ahyxkyz6m7nk774g3x5amfrcznn06fw5zywedxtqg2hw8cgf9gks2rhgnl',
+      counterparties: [
+        {
+          chainID: 'injective-888',
+          sourceChannel: 'channel-1524',
+        },
+        {
+          chainID: 'side-devnet-1',
+          sourceChannel: 'channel-1523',
+        },
+      ],
     },
-  };
+    testnet: {
+      contract:
+        'osmo18z2ge4vfvnh3wj2yz9f92q9azdrhjf84vct6yzesgcdkp6j2579qxsplh8',
+      counterparties: [
+        {
+          chainID: 'injective-888',
+          sourceChannel: 'channel-?',
+        },
+        {
+          chainID: 'side-testnet-1',
+          sourceChannel: 'channel-?',
+        },
+      ],
+    },
+    main: {},
+  },
+  injective: {
+    dev: {
+      contract: 'inj13gttmee75m22058kcnsua3yq8uhk9lwkmyurer',
+      counterparties: [
+        {
+          chainID: 'osmo-test-5',
+          sourceChannel: 'channel-100',
+        },
+        {
+          chainID: 'side-devnet-1',
+          sourceChannel: 'channel-99',
+        },
+      ],
+    },
+    testnet: {
+      contract: 'inj1qyvln72dzdadk8jrx45upwjeax55gdaxudxmsc',
+      counterparties: [
+        {
+          chainID: 'osmo-test-5',
+          sourceChannel: 'channel-?',
+        },
+        {
+          chainID: 'side-testnet-1',
+          sourceChannel: 'channel-102',
+        },
+      ],
+    },
+    main: {},
+  },
+};
---
+++ shambhala/ics101/config.ts
@@ -0,0 +1,86 @@
+/**
+ * 
+ * dev : dev development environment
+ * 
+ * dev|testnet|main : {
+ *  
+ *    contract: Contract address;
+ *    counterparties:[
+ *      {
+ *        channelId: counterparty channelId
+ *      }
+ *    ]
+ * 
+ * }
+ */
+
+const config = {
+    sideChain: {
+      dev: {
+      },
+      testnet: {
+        contract:
+        'side1ghd753shjuwexxywmgs4xz7x2q732vcnkm6h2pyv9s6ah3hylvrqs4af7r',
+        counterparties: [
+            {
+                chainID: 'osmo-test-5',
+                sourceChannel: 'channel-1',
+            },
+            {
+                chainID: 'injective-888',
+                sourceChannel: 'channel-101',
+            },
+            {
+                chainID: 'constantine-3',
+                sourceChannel: 'channel-3',
+            },
+        ],
+      },
+      main: {},
+    },
+    osmoChain: {
+      testnet: {
+        contract:
+          'osmo1ekyf3lqyrjep0syhwxgkgqfzqrjthtt83g7lvkmqurlkjxjj45hq8dcag3',
+        counterparties: [
+          {
+            chainID: 'injective-888',
+            sourceChannel: 'channel-1547',
+          },
+          {
+            chainID: 'side-testnet-1',
+            sourceChannel: 'channel-1491',
+          },
+        ],
+      },
+      main: {},
+    },
+    injective: {
+      testnet: {
+        contract: 'inj1qpd2gj05900za4auvz3p26ujfza2h67x5tfkn8',
+        counterparties: [
+          {
+            chainID: 'osmo-test-5',
+            sourceChannel: 'channel-103',
+          },
+          {
+            chainID: 'side-testnet-1',
+            sourceChannel: 'channel-101',
+          },
+        ],
+      },
+      main: {},
+    },
+    archway: {
+        testnet: {
+          contract: 'archway1dquhcfju4rpt8whvlvx956mdzcsnyfjzaxn3rq26vtmj747xt0rquvrc2p',
+          counterparties: [
+            {
+              chainID: 'side-testnet-1',
+              sourceChannel: 'channel-31',
+            },
+          ],
+        },
+        main: {},
+      },
+  };
